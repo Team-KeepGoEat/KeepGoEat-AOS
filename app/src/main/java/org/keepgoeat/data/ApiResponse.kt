ecfce3124c2954614bf32ae243581a3b41be519e
youngjinc <cyjin6789@gmail.com>
2022-12-31 20:07:43 +0900

    [add] #2 주석 추가

--- app/src/main/java/org/keepgoeat/data/ApiResponse.kt
+++ app/src/main/java/org/keepgoeat/data/ApiResponse.kt
@@ -1,6 +1,6 @@
 package org.keepgoeat.data
 
-sealed class ApiResponse<out T>(val data: T?) {
+sealed class ApiResponse<out T>(val data: T?) { // TODO 수정
     data class Idle<T>(val datas: T?) : ApiResponse<T>(null)
     data class Loading<T>(val datas: T?) : ApiResponse<T>(datas)
     data class Success<T>(val datas: T) : ApiResponse<T>(datas)
