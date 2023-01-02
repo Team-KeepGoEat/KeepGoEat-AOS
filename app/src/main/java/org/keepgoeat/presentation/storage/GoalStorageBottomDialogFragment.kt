e503426013a9c210ba9278f5b12fa6ed7d69561a
youngjinc <cyjin6789@gmail.com>
2023-01-02 17:13:31 +0900

    [feat] #14 eatingType에 따른 목표 보관 바텀시트 분기 처리

--- app/src/main/java/org/keepgoeat/presentation/storage/GoalStorageBottomDialogFragment.kt
+++ app/src/main/java/org/keepgoeat/presentation/storage/GoalStorageBottomDialogFragment.kt
@@ -2,19 +2,21 @@ package org.keepgoeat.presentation.storage
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.keepgoeat.R
 import org.keepgoeat.databinding.DialogBottomGoalStorageBinding
+import org.keepgoeat.presentation.detail.GoalDetailViewModel
 import org.keepgoeat.util.binding.BindingBottomSheetDialogFragment
 
 @AndroidEntryPoint
 class GoalStorageBottomDialogFragment :
     BindingBottomSheetDialogFragment<DialogBottomGoalStorageBinding>(R.layout.dialog_bottom_goal_storage) {
-    private val viewModel: GoalStorageViewModel by viewModels()
+    private val viewModel: GoalDetailViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.viewModel = viewModel
     }
 }
