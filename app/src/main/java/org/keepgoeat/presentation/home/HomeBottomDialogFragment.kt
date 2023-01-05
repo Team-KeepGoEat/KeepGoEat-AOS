5f062b8536bc8b07b6ef85621322e333fbcc16bc
YuBeen-Park <park_ubin@naver.com>
2023-01-05 23:47:09 +0900

    [feat] #35 Home -> GoalSettingActivity로 이동 구현

--- app/src/main/java/org/keepgoeat/presentation/home/HomeBottomDialogFragment.kt
+++ app/src/main/java/org/keepgoeat/presentation/home/HomeBottomDialogFragment.kt
@@ -1,19 +1,33 @@
 package org.keepgoeat.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import org.keepgoeat.R
 import org.keepgoeat.databinding.DialogBottomHomeBinding
+import org.keepgoeat.presentation.setting.GoalSettingActivity
+import org.keepgoeat.presentation.type.EatingType
 import org.keepgoeat.util.binding.BindingBottomSheetDialogFragment
 
-class HomeBottomDialogFragment : BindingBottomSheetDialogFragment<DialogBottomHomeBinding>(R.layout.dialog_bottom_home) {
+class HomeBottomDialogFragment :
+    BindingBottomSheetDialogFragment<DialogBottomHomeBinding>(R.layout.dialog_bottom_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addListeners()
     }
 
     private fun addListeners() {
-        binding.layoutHomeBottomMore.setOnClickListener {}
-        binding.layoutHomeBottomLess.setOnClickListener {}
+        binding.layoutHomeBottomMore.setOnClickListener {
+            changeActivity(EatingType.MORE)
+        }
+        binding.layoutHomeBottomLess.setOnClickListener {
+            changeActivity(EatingType.LESS)
+        }
+    }
+
+    private fun changeActivity(eatingType: EatingType) {
+        val intent = Intent(requireActivity(), GoalSettingActivity::class.java)
+        intent.putExtra(GoalSettingActivity.ARG_EATING_TYPE, eatingType.name)
+        startActivity(intent)
     }
 }
