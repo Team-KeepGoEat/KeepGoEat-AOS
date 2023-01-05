d09cc153286aa1b02529ad296c9a73550e197b66
youngjinc <cyjin6789@gmail.com>
2023-01-05 23:10:54 +0900

    [feat] #20 마이페이지 > 헤더뷰 버튼 클릭 시 볼드체 및 컬러 적용

--- app/src/main/java/org/keepgoeat/presentation/my/MyHeaderAdapter.kt
+++ app/src/main/java/org/keepgoeat/presentation/my/MyHeaderAdapter.kt
@@ -2,7 +2,9 @@ package org.keepgoeat.presentation.my
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.keepgoeat.R
 import org.keepgoeat.databinding.LayoutMyHeaderBinding
 import org.keepgoeat.presentation.type.EatingType
 
@@ -21,24 +23,36 @@ class MyHeaderAdapter(private val eatingTypeClickListener: ((EatingType?) -> Uni
     }
 
     override fun onBindViewHolder(holder: MyHeaderViewHolder, position: Int) {
-        holder.onBind(eatingTypeClickListener)
+        holder.onBind(eatingTypeClickListener, ::changeTextAppearance)
     }
 
     override fun getItemCount() = 1
 
+    private fun changeTextAppearance(clickedView: TextView, unclickedView1: TextView, unclickedView2: TextView) {
+        clickedView.setTextAppearance(R.style.TextAppearance_System5_Bold)
+        unclickedView1.setTextAppearance(R.style.TextAppearance_System5)
+        unclickedView2.setTextAppearance(R.style.TextAppearance_System5)
+        clickedView.setTextColor(clickedView.context.getColor(R.color.gray_800))
+        unclickedView1.setTextColor(clickedView.context.getColor(R.color.gray_400))
+        unclickedView2.setTextColor(clickedView.context.getColor(R.color.gray_400))
+    }
+
     class MyHeaderViewHolder(
         private val binding: LayoutMyHeaderBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(eatingTypeClickListener: ((EatingType?) -> Unit)) {
+        fun onBind(eatingTypeClickListener: ((EatingType?) -> Unit), changeTextAppearance: (TextView, TextView, TextView) -> Unit) {
             with(binding) {
                 tvAll.setOnClickListener {
                     eatingTypeClickListener(null)
+                    changeTextAppearance(tvAll, tvMore, tvLess)
                 }
                 tvMore.setOnClickListener {
                     eatingTypeClickListener(EatingType.MORE)
+                    changeTextAppearance(tvMore, tvAll, tvLess)
                 }
                 tvLess.setOnClickListener {
                     eatingTypeClickListener(EatingType.LESS)
+                    changeTextAppearance(tvLess, tvAll, tvMore)
                 }
             }
         }
