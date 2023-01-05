e8861f986c506bf2ed06f6f1d16ea5394642047e
YuBeen-Park <park_ubin@naver.com>
2023-01-05 18:19:26 +0900

    [chore] #30 HomeMyGoal 변수명 변경

--- app/src/main/java/org/keepgoeat/domain/model/HomeMyGoal.kt
+++ app/src/main/java/org/keepgoeat/domain/model/HomeMyGoal.kt
@@ -3,8 +3,8 @@ package org.keepgoeat.domain.model
 import org.keepgoeat.presentation.type.HomeGoalViewType
 
 data class HomeMyGoal(
-    val goalId: Int,
-    val goalContent: String,
+    val id: Int,
+    val goalTitle: String,
     val isMore: Boolean,
     val isAchieved: Boolean,
     val thisMonthCount: Int,
