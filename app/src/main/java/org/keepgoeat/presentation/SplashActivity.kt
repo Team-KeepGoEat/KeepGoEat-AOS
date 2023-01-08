8396f75eb9f73612b97e076396b03cb365c5238d
Daeun <eldms0406@naver.com>
2023-01-09 00:26:33 +0900

    [mod] #40 스플래시 activity에서 BindingActivity 상속 받는걸로 변경

--- app/src/main/java/org/keepgoeat/presentation/SplashActivity.kt
+++ app/src/main/java/org/keepgoeat/presentation/SplashActivity.kt
@@ -2,23 +2,23 @@ package org.keepgoeat.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.keepgoeat.R
+import org.keepgoeat.databinding.ActivitySplashBinding
 import org.keepgoeat.presentation.onboarding.OnboardingActivity
 import org.keepgoeat.presentation.sign.SignActivity
 import org.keepgoeat.presentation.sign.SignSharedPreferences
+import org.keepgoeat.util.binding.BindingActivity
 
 @AndroidEntryPoint
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
         loadSplashScreen()
     }
 
