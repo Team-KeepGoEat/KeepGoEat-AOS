e54b681f8664f02423dbab467657edf6b2fb5fd8
YuBeen-Park <park_ubin@naver.com>
2023-01-08 15:23:29 +0900

    [chore] #61 액티비티 이동 함수명 변경

--- app/src/main/java/org/keepgoeat/presentation/home/HomeBottomDialogFragment.kt
+++ app/src/main/java/org/keepgoeat/presentation/home/HomeBottomDialogFragment.kt
@@ -18,16 +18,16 @@ class HomeBottomDialogFragment :
 
     private fun addListeners() {
         binding.layoutHomeBottomMore.setOnClickListener {
-            moveActivity(EatingType.MORE)
+            moveToSetting(EatingType.MORE)
             dismiss()
         }
         binding.layoutHomeBottomLess.setOnClickListener {
-            moveActivity(EatingType.LESS)
+            moveToSetting(EatingType.LESS)
             dismiss()
         }
     }
 
-    private fun moveActivity(eatingType: EatingType) {
+    private fun moveToSetting(eatingType: EatingType) {
         val intent = Intent(requireActivity(), GoalSettingActivity::class.java)
         intent.putExtra(GoalSettingActivity.ARG_EATING_TYPE, eatingType.name)
         startActivity(intent)
