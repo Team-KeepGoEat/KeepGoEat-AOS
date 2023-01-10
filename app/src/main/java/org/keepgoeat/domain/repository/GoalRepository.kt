1578ce903d82d33aa3429e717c0ff4813df37a83
YuBeen-Park <park_ubin@naver.com>
2023-01-10 23:46:22 +0900

    [feat] #85 목표 삭제 api 메소드 구현

--- app/src/main/java/org/keepgoeat/domain/repository/GoalRepository.kt
+++ app/src/main/java/org/keepgoeat/domain/repository/GoalRepository.kt
@@ -1,18 +1,26 @@
 package org.keepgoeat.domain.repository
 
-import org.keepgoeat.data.model.response.ResponseGoalAchievement
-import org.keepgoeat.data.model.response.ResponseGoalContent
-import org.keepgoeat.data.model.response.ResponseGoalDetail
-import org.keepgoeat.data.model.response.ResponseGoalKeep
-import org.keepgoeat.data.model.response.ResponseHome
+import org.keepgoeat.data.model.response.*
 
 interface GoalRepository {
     suspend fun fetchHomeEntireData(): ResponseHome.HomeData?
-    suspend fun achieveGoal(goalId: Int, isAchieved: Boolean): ResponseGoalAchievement.ResponseGoalAchievementData?
+    suspend fun achieveGoal(
+        goalId: Int,
+        isAchieved: Boolean
+    ): ResponseGoalAchievement.ResponseGoalAchievementData?
 
     // TODO update -> add 네이밍 수정 필요
-    suspend fun uploadGoalContent(title: String, isMore: Boolean): ResponseGoalContent.ResponseGoalContentData?
-    suspend fun editGoalContent(id: Int, title: String): ResponseGoalContent.ResponseGoalContentData?
+    suspend fun uploadGoalContent(
+        title: String,
+        isMore: Boolean
+    ): ResponseGoalContent.ResponseGoalContentData?
+
+    suspend fun editGoalContent(
+        id: Int,
+        title: String
+    ): ResponseGoalContent.ResponseGoalContentData?
+
     suspend fun fetchGoalDetail(goalId: Int): ResponseGoalDetail.ResponseGoalDetailData?
     suspend fun keepGoal(id: Int): ResponseGoalKeep.ResponseGoalKeepData?
+    suspend fun deleteGoal(id: Int): ResponseGoalDeleted.ResponseGoalDeletedData?
 }
