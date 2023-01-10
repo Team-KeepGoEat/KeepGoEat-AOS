1ca0f90cdb45fe5d20c8f91a8d226c1a46f48b18
YuBeen-Park <park_ubin@naver.com>
2023-01-10 23:45:57 +0900

    [feat] #85 목표 삭제 api response 데이터클래스 구현

---
+++ app/src/main/java/org/keepgoeat/data/model/response/ResponseGoalDeleted.kt
@@ -0,0 +1,16 @@
+package org.keepgoeat.data.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseGoalDeleted(
+    val data: ResponseGoalDeletedData?,
+    val message: String,
+    val status: Int,
+    val success: Boolean
+) {
+    @Serializable
+    data class ResponseGoalDeletedData(
+        val goalId: Int
+    )
+}
