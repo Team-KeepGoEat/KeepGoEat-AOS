171411eb828b0dd6b1a12ee6efaf7dcaed232d8d
youngjinc <cyjin6789@gmail.com>
2023-01-11 15:45:32 +0900

    [add] #66 로그인 response 추가

---
+++ app/src/main/java/org/keepgoeat/data/model/request/RequestAuth.kt
@@ -0,0 +1,9 @@
+package org.keepgoeat.data.model.request
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestAuth(
+    val platformAccessToken: String,
+    val platform: String,
+)
