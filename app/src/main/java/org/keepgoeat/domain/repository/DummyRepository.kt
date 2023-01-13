037c67f634d07f1b2826e7e3b360bc9b90814f94
youngjinc <cyjin6789@gmail.com>
2023-01-13 19:08:28 +0900

    [del] #113 불필요한 더미 파일 삭제

--- app/src/main/java/org/keepgoeat/domain/repository/DummyRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.keepgoeat.domain.repository
-
-import org.keepgoeat.data.model.response.ResponseListResult
-
-interface DummyRepository {
-    suspend fun uploadDummy(name: String, email: String): ResponseListResult?
-}
