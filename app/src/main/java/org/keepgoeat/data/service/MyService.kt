a45a4c79895831c86ff53ab2ae10dd4e142866eb
youngjinc <cyjin6789@gmail.com>
2023-02-06 22:24:16 +0900

    [refactor] #140 마이페이지뷰 response를 Result로 반환

--- app/src/main/java/org/keepgoeat/data/service/MyService.kt
+++ app/src/main/java/org/keepgoeat/data/service/MyService.kt
@@ -1,13 +1,12 @@
 package org.keepgoeat.data.service
 
 import org.keepgoeat.data.model.response.ResponseMy
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MyService {
     @GET("mypage")
     suspend fun fetchMyData(
-        @Query("sort") value: String
-    ): Response<ResponseMy>
+        @Query("sort") value: String,
+    ): ResponseMy
 }
