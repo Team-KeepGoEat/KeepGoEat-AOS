69afda87630b161a98b54ee83d21bffacaf253a2
YuBeen-Park <park_ubin@naver.com>
2023-02-06 16:45:40 +0900

    [feat] #112 DispatcherModule (di) 추가

---
+++ app/src/main/java/org/keepgoeat/di/DispatcherModule.kt
@@ -0,0 +1,22 @@
+package org.keepgoeat.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+}
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
