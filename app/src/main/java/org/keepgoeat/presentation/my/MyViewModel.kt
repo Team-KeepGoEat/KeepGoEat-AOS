bc69115d09aff1cee669b7e9c76c1bb0ec40f0ad
youngjinc <cyjin6789@gmail.com>
2023-02-06 22:24:43 +0900

    [refactor] #140 마이페이지뷰 Livedata -> Flow로 마이그레이션

--- app/src/main/java/org/keepgoeat/presentation/my/MyViewModel.kt
+++ app/src/main/java/org/keepgoeat/presentation/my/MyViewModel.kt
@@ -1,20 +1,21 @@
 package org.keepgoeat.presentation.my
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.keepgoeat.domain.model.MyGoal
 import org.keepgoeat.domain.repository.MyRepository
 import org.keepgoeat.presentation.type.SortType
+import org.keepgoeat.util.UiState
 import javax.inject.Inject
 
 @HiltViewModel
 class MyViewModel @Inject constructor(private val myRepository: MyRepository) : ViewModel() {
-    private val _goalList = MutableLiveData<List<MyGoal>>()
-    val goalList: LiveData<List<MyGoal>> get() = _goalList
+    private val _goalList = MutableStateFlow<UiState<List<MyGoal>>>(UiState.Loading)
+    val goalList get() = _goalList.asStateFlow()
 
     init {
         fetchAchievedGoalBySort(SortType.ALL)
@@ -22,9 +23,12 @@ class MyViewModel @Inject constructor(private val myRepository: MyRepository) : 
 
     fun fetchAchievedGoalBySort(sortType: SortType) {
         viewModelScope.launch {
-            myRepository.fetchMyData(sortType.name.lowercase())?.let { MyData ->
-                _goalList.value = MyData.toMyGoal()
-            }
+            myRepository.fetchMyData(sortType.name.lowercase())
+                .onSuccess {
+                    _goalList.value = UiState.Success(it)
+                }.onFailure {
+                    _goalList.value = UiState.Error(it.message)
+                }
         }
     }
 }
