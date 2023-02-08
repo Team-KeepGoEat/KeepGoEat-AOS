9d3af9a705daf9e0be26b28ec2660c9ec08724eb
Daeun <eldms0406@naver.com>
2023-02-08 20:55:31 +0900

    [fix] #148 머지 conflict 해결

--- app/src/main/java/org/keepgoeat/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/org/keepgoeat/data/repository/AuthRepositoryImpl.kt
@@ -1,59 +1,31 @@
 package org.keepgoeat.data.repository
 
-import org.keepgoeat.data.ApiResult
 import org.keepgoeat.data.datasource.local.KGEDataSource
 import org.keepgoeat.data.datasource.remote.AuthDataSource
 import org.keepgoeat.data.model.request.RequestAuth
 import org.keepgoeat.data.model.response.ResponseAuth
 import org.keepgoeat.data.model.response.ResponseRefresh
 import org.keepgoeat.domain.repository.AuthRepository
-import timber.log.Timber
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authDataSource: AuthDataSource,
     private val localStorage: KGEDataSource
 ) : AuthRepository {
-    override suspend fun login(requestAuth: RequestAuth): ResponseAuth.ResponseAuthData? {
-        val result = authDataSource.login(requestAuth)
-        return when (result) {
-            is ApiResult.Success -> {
-                val response = result.data?.data
-                with(localStorage) {
-                    isLogin = true
-                    response?.let {
-                        accessToken = it.accessToken
-                        refreshToken = it.refreshToken
-                    }
+    override suspend fun login(requestAuth: RequestAuth): Result<ResponseAuth.ResponseAuthData?> =
+        kotlin.runCatching {
+            authDataSource.login(requestAuth).data
+        }.onSuccess {
+            with(localStorage) {
+                isLogin = true
+                it?.let {
+                    accessToken = it.accessToken
+                    refreshToken = it.refreshToken
                 }
-                Timber.d(result.data?.message)
-                result.data?.data
-            }
-            is ApiResult.NetworkError -> {
-                Timber.d("Network Error")
-                null
-            }
-            is ApiResult.GenericError -> {
-                Timber.d("(${result.code}): ${result.message}")
-                null
             }
         }
-    }
 
-    override suspend fun refresh(): ResponseRefresh.ResponseToken? {
-        val result = authDataSource.refresh()
-        return when (result) {
-            is ApiResult.Success -> {
-                result.data?.data
-            }
-            is ApiResult.NetworkError -> {
-                Timber.d("Network Error")
-                null
-            }
-            is ApiResult.GenericError -> {
-                Timber.d("(${result.code}): ${result.message}")
-                null
-            }
-        }
+    override suspend fun refresh(): Result<ResponseRefresh.ResponseToken?> = runCatching {
+        authDataSource.refresh().data
     }
 }
