810fc926fb57c713a3fc6841c34aef1a75c25e38
Daeun <eldms0406@naver.com>
2023-02-09 18:53:34 +0900

    [mod] #148 네이버 로그인 성공했을 때 로직 수정

--- app/src/main/java/org/keepgoeat/data/service/AuthService.kt
+++ app/src/main/java/org/keepgoeat/data/service/AuthService.kt
@@ -3,15 +3,14 @@ package org.keepgoeat.data.service
 import org.keepgoeat.data.model.request.RequestAuth
 import org.keepgoeat.data.model.response.ResponseAuth
 import org.keepgoeat.data.model.response.ResponseRefresh
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("auth")
-    suspend fun login(@Body request: RequestAuth): Response<ResponseAuth>
+    suspend fun login(@Body request: RequestAuth): ResponseAuth
 
     @GET("auth/refresh")
-    suspend fun refresh(): Response<ResponseRefresh>
+    suspend fun refresh(): ResponseRefresh
 }
