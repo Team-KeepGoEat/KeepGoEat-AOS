9c3856003b598fc18c4fe629be3e0910c47c1d04
youngjinc <cyjin6789@gmail.com>
2023-02-15 18:46:24 +0900

    [feat] #154 로그아웃 다이얼로그 구현

---
+++ app/src/main/java/org/keepgoeat/presentation/my/LogoutDialogFragment.kt
@@ -0,0 +1,47 @@
+package org.keepgoeat.presentation.my
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import dagger.hilt.android.AndroidEntryPoint
+import org.keepgoeat.R
+import org.keepgoeat.data.service.KakaoAuthService
+import org.keepgoeat.data.service.NaverAuthService
+import org.keepgoeat.databinding.DialogLogoutBinding
+import org.keepgoeat.presentation.type.SocialLoginType
+import org.keepgoeat.util.binding.BindingDialogFragment
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class LogoutDialogFragment : BindingDialogFragment<DialogLogoutBinding>(R.layout.dialog_logout) {
+    @Inject
+    lateinit var kakaoSignService: KakaoAuthService
+
+    @Inject
+    lateinit var naverSignService: NaverAuthService
+
+    private val viewModel: MyViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        addListeners()
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.yes.setOnClickListener {
+            when (viewModel.loginPlatForm) {
+                SocialLoginType.NAVER -> naverSignService.logoutNaver(viewModel::logout)
+                SocialLoginType.KAKAO -> kakaoSignService.logoutKakao(viewModel::logout)
+                else -> {}
+            }
+            dismiss()
+        }
+        binding.no.setOnClickListener {
+            dismiss()
+        }
+    }
+}
