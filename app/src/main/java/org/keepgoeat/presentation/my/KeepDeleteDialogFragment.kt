a749b2bece0c5405038ef255d30dfb04206c867b
Daeun <eldms0406@naver.com>
2023-02-28 23:05:16 +0900

    [feat] #175 보관함 목표 삭제 다이얼로그 띄우는거까지 구현

---
+++ app/src/main/java/org/keepgoeat/presentation/my/KeepDeleteDialogFragment.kt
@@ -0,0 +1,24 @@
+package org.keepgoeat.presentation.my
+
+import android.os.Bundle
+import android.view.View
+import org.keepgoeat.R
+import org.keepgoeat.databinding.DialogKeepDeleteBinding
+import org.keepgoeat.util.binding.BindingDialogFragment
+
+class KeepDeleteDialogFragment :
+    BindingDialogFragment<DialogKeepDeleteBinding>(R.layout.dialog_keep_delete) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addListeners()
+    }
+
+    private fun addListeners() {
+        binding.yes.setOnClickListener {
+            //TODO 목표 삭제 api 연동
+        }
+        binding.no.setOnClickListener {
+            dismiss()
+        }
+    }
+}
