6c3473f86d159af69e1bbda89c47fb0720afdfeb
YuBeen-Park <park_ubin@naver.com>
2023-02-28 18:24:42 +0900

    [mod] #172 경고 메시지 생성 후 text 입력시 경고 메시지 사라지도록 변경

--- app/src/main/java/org/keepgoeat/presentation/withdraw/WithdrawActivity.kt
+++ app/src/main/java/org/keepgoeat/presentation/withdraw/WithdrawActivity.kt
@@ -1,6 +1,8 @@
 package org.keepgoeat.presentation.withdraw
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.activity.viewModels
 import org.keepgoeat.R
@@ -62,6 +64,14 @@ class WithdrawActivity : BindingActivity<ActivityWithdrawBinding>(R.layout.activ
                     binding.clickType = WithdrawCheckType.CLICKED
             }
         }
+        binding.etOtherReason.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!binding.etOtherReason.text.isNullOrBlank())
+                    binding.tvWithdrawEdittextError.visibility = View.GONE
+            }
+        })
         binding.layoutWithdraw.setOnClickListener { // 외부 영역 클릭 했을 때
             clearFocus()
         }
