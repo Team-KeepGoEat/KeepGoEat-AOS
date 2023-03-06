9dc4255627ecc535cbd0e1a88057bb5b9d619c4c
Daeun <eldms0406@naver.com>
2023-03-06 15:32:07 +0900

    [feat] #175 삭제 dialog와 viewModel 연결

--- app/src/main/java/org/keepgoeat/presentation/my/KeepDeleteDialogFragment.kt
+++ app/src/main/java/org/keepgoeat/presentation/my/KeepDeleteDialogFragment.kt
@@ -2,14 +2,18 @@ package org.keepgoeat.presentation.my
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import org.keepgoeat.R
 import org.keepgoeat.databinding.DialogKeepDeleteBinding
 import org.keepgoeat.util.binding.BindingDialogFragment
 
 class KeepDeleteDialogFragment :
     BindingDialogFragment<DialogKeepDeleteBinding>(R.layout.dialog_keep_delete) {
+    private val viewModel: MyViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
         addListeners()
     }
 
