529680a78e8417824ee39e09eb98171ade791678
Dan2dani <eldms0406@naver.com>
2023-03-27 08:40:03 +0900

    [mod] #204 goalFood null로 초기화 및 특수문자 관련 유효성 기준 삭제

--- app/src/main/java/org/keepgoeat/presentation/setting/GoalSettingActivity.kt
+++ app/src/main/java/org/keepgoeat/presentation/setting/GoalSettingActivity.kt
@@ -2,7 +2,6 @@ package org.keepgoeat.presentation.setting
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
@@ -66,11 +65,6 @@ class GoalSettingActivity :
                 is UiState.Error -> {}
                 else -> {}
             }
-        }.launchIn(lifecycleScope)
-
-        viewModel.goalFood.flowWithLifecycle(lifecycle).onEach { goalFood ->
-            if (goalFood.length > 0)
-                binding.tvGoalTitleLengthGuide.visibility = View.VISIBLE
         }.launchIn(lifecycleScope)
     }
 
