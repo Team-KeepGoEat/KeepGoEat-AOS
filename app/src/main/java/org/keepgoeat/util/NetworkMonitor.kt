b85be679ae73fd482b91513711e33187f882eba3
youngjinc <cyjin6789@gmail.com>
2023-03-29 15:21:40 +0900

    [fix] #212 네트워크 연결 상태에서도 종종 네트워크 에러뷰가 보이는 버그 수정

--- app/src/main/java/org/keepgoeat/util/NetworkMonitor.kt
+++ app/src/main/java/org/keepgoeat/util/NetworkMonitor.kt
@@ -3,26 +3,19 @@ package org.keepgoeat.util
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.callbackFlow
 import org.keepgoeat.util.extension.toStateFlow
 
+/* ref :
+https://developer.android.com/training/basics/network-ops/reading-network-state
+https://medium.com/@bazzairvine/observing-your-network-connection-with-flow-1cdedf31780c */
+
 class NetworkMonitor(private val context: Context, private val coroutineScope: CoroutineScope) {
     private var connectivityManager: ConnectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-    private val request = NetworkRequest.Builder()
-        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-        .apply {
-            addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-        }
-        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-        .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-        .build()
 
     val isConnected: StateFlow<Boolean> = callbackFlow {
         val callback = object : ConnectivityManager.NetworkCallback() {
@@ -37,7 +30,7 @@ class NetworkMonitor(private val context: Context, private val coroutineScope: C
             }
         }
 
-        connectivityManager.registerNetworkCallback(request, callback)
+        connectivityManager.registerDefaultNetworkCallback(callback)
 
         awaitClose {
             connectivityManager.unregisterNetworkCallback(callback)
