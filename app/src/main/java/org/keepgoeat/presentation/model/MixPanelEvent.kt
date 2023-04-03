3f296b0d26582afcb05a4bdb4020b23faef6dc92
youngjinc <cyjin6789@gmail.com>
2023-04-03 20:37:39 +0900

    [feat] #214 믹스패널 초기 세팅

---
+++ app/src/main/java/org/keepgoeat/presentation/model/MixPanelEvent.kt
@@ -0,0 +1,6 @@
+package org.keepgoeat.presentation.model
+
+data class MixPanelEvent(
+    val name: String,
+    val params: Map<String, Any>?
+)
---
+++ app/src/main/java/org/keepgoeat/util/mixpanel/MixpanelProvider.kt
@@ -0,0 +1,46 @@
+package org.keepgoeat.util.mixpanel
+
+import android.content.Context
+import com.mixpanel.android.mpmetrics.MixpanelAPI
+import org.json.JSONObject
+import org.keepgoeat.BuildConfig
+import org.keepgoeat.data.datasource.local.KGEDataSource
+import org.keepgoeat.presentation.model.MixPanelEvent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MixpanelProvider @Inject constructor(private val localStorage: KGEDataSource) {
+    private lateinit var instance: MixpanelAPI
+
+    fun initialize(context: Context) {
+        instance = MixpanelAPI.getInstance(context, BuildConfig.MIXPANEL_PROJECT_TOKEN, true)
+    }
+
+    fun setUser() {
+        instance.identify(localStorage.userEmail)
+        instance.people.set(getUserProperties())
+    }
+
+    private fun getUserProperties() = JSONObject().apply {
+        put(PROPERTY_NICKNAME, localStorage.userName)
+        put(PROPERTY_EMAIL, localStorage.userEmail)
+    }
+
+    /** 믹스패널에 이벤트를 전송하는 함수. 유저 프로퍼티를 함께 전송해야하는 경우, isRequiredUserProps를 false로 설정 필요 */
+    fun sendEvent(event: MixPanelEvent, isRequiredUserProps: Boolean = true) {
+        val props = if (isRequiredUserProps) getUserProperties() else JSONObject()
+
+        event.params?.let {
+            for ((key, value) in it)
+                props.put(key, value)
+        }
+
+        instance.track(event.name, props)
+    }
+
+    companion object {
+        private const val PROPERTY_NICKNAME = "\$name"
+        private const val PROPERTY_EMAIL = "\$email"
+    }
+}
