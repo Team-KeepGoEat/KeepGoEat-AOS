087c9781967c969e753f69456cee964321e46c86
youngjinc <cyjin6789@gmail.com>
2023-04-19 22:15:29 +0900

    [del] #214 불필요한 import 제거

--- app/src/main/java/org/keepgoeat/util/mixpanel/SignEvent.kt
+++ app/src/main/java/org/keepgoeat/util/mixpanel/SignEvent.kt
@@ -1,7 +1,6 @@
 package org.keepgoeat.util.mixpanel
 
 import org.keepgoeat.presentation.model.MixPanelEvent
-import org.keepgoeat.presentation.type.LoginPlatformType
 
 object SignEvent {
     fun completeLogin() = MixPanelEvent("Login", null)
