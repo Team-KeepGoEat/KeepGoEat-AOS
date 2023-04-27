dc64f863098a675a3fb76594132dfa4b5d12b59d
youngjinc <cyjin6789@gmail.com>
2023-04-27 21:14:34 +0900

    [refactor] #228 온보딩 관련 코드 정리

--- app/src/main/java/org/keepgoeat/presentation/type/OnBoardingViewType.kt
+++ app/src/main/java/org/keepgoeat/presentation/type/OnBoardingViewType.kt
@@ -4,8 +4,22 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import org.keepgoeat.R
 
-enum class OnBoardingViewType(@StringRes val titleStrRes: Int, @StringRes val desStrRes: Int, @DrawableRes val imageRes: Int) {
-    FIRST(R.string.onboarding1_title, R.string.onboarding1_des, R.drawable.img_onboarding_1),
-    SECOND(R.string.onboarding2_title, R.string.onboarding2_des, R.drawable.img_onboarding_2),
-    THIRD(R.string.onboarding3_title, R.string.onboarding3_des, R.drawable.img_onboarding_3)
+enum class OnBoardingViewType(
+    @StringRes val titleStrRes: Int,
+    @StringRes val desStrRes: Int,
+    @StringRes val btnTextRes: Int,
+    @DrawableRes val imageRes: Int,
+) {
+    FIRST(R.string.onboarding1_title,
+        R.string.onboarding1_des,
+        R.string.onboarding1_button,
+        R.drawable.img_onboarding_1),
+    SECOND(R.string.onboarding2_title,
+        R.string.onboarding2_des,
+        R.string.onboarding2_button,
+        R.drawable.img_onboarding_2),
+    THIRD(R.string.onboarding3_title,
+        R.string.onboarding3_des,
+        R.string.onboarding3_button,
+        R.drawable.img_onboarding_3)
 }
