f909f8e6abbf24bba125f299c5008cc5d58c67cd
YuBeen-Park <park_ubin@naver.com>
2023-05-02 21:01:02 +0900

    [feat] #230 이모지 1개씩 count 되는 로직 구현

---
+++ app/src/main/java/org/keepgoeat/util/extension/StringExt.kt
@@ -0,0 +1,13 @@
+package org.keepgoeat.util.extension
+
+import java.text.BreakIterator
+
+fun String?.getGraphemeLength(): Int {
+    val it: BreakIterator = BreakIterator.getCharacterInstance()
+    it.setText(this)
+    var count = 0
+    while (it.next() != BreakIterator.DONE) {
+        count++
+    }
+    return count
+}
