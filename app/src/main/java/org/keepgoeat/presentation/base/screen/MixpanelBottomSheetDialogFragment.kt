87a82466b3d7b06f47736e1c8793c17bfba4b756
youngjinc <cyjin6789@gmail.com>
2023-05-03 10:01:03 +0900

    [chore] #228 코드 정리

--- app/src/main/java/org/keepgoeat/presentation/base/screen/MixpanelBottomSheetDialogFragment.kt
+++ app/src/main/java/org/keepgoeat/presentation/base/screen/MixpanelBottomSheetDialogFragment.kt
@@ -1,32 +1,14 @@
 package org.keepgoeat.presentation.base.screen
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.keepgoeat.presentation.base.viewmodel.MixpanelViewModel
 
 abstract class MixpanelBottomSheetDialogFragment<B : ViewDataBinding>(
-    @LayoutRes private val layoutResId: Int,
+    @LayoutRes private val layoutRes: Int,
     private val screenName: String,
-) :
-    BottomSheetDialogFragment() {
-    private var _binding: B? = null
-    val binding get() = requireNotNull(_binding!!) { "${this::class.java.simpleName}에서 에러가 발생했습니다." }
+) : BindingBottomSheetDialogFragment<B>(layoutRes) {
     abstract val viewModel: MixpanelViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
-        return binding.root
-    }
 
     override fun onStart() {
         super.onStart()
@@ -36,10 +18,5 @@ abstract class MixpanelBottomSheetDialogFragment<B : ViewDataBinding>(
     override fun onStop() {
         super.onStop()
         viewModel.stopRecodingScreenTime(screenName)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
