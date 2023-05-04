ceb5429c7adbe01a9f0676ec4ddaeb5690c8b73c
YuBeen-Park <park_ubin@naver.com>
2023-05-05 00:28:56 +0900

    [chore] #230 주석달기

--- app/src/main/java/org/keepgoeat/util/extension/StringExt.kt
+++ app/src/main/java/org/keepgoeat/util/extension/StringExt.kt
@@ -2,6 +2,7 @@ package org.keepgoeat.util.extension
 
 import java.text.BreakIterator
 
+/** 사용자가 인식하는 문자열의 글자 수를 반환하는 함수 */
 fun String?.getStringLength(): Int {
     if (this == null) return 0
     val it: BreakIterator = BreakIterator.getCharacterInstance()
