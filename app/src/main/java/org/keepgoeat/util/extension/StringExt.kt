54ef35661fb5d449130245457b84f66320defe7a
YuBeen-Park <park_ubin@naver.com>
2023-05-05 00:21:20 +0900

    [mod] #230 stringExt null 처리하기

--- app/src/main/java/org/keepgoeat/util/extension/StringExt.kt
+++ app/src/main/java/org/keepgoeat/util/extension/StringExt.kt
@@ -3,6 +3,7 @@ package org.keepgoeat.util.extension
 import java.text.BreakIterator
 
 fun String?.getStringLength(): Int {
+    if (this == null) return 0
     val it: BreakIterator = BreakIterator.getCharacterInstance()
     it.setText(this)
     var count = 0
