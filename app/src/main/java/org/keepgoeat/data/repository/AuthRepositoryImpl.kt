3689e215718206178026cb1d895397dfd7654df6
youngjinc <cyjin6789@gmail.com>
2023-05-05 13:47:32 +0900

    [mod] #236 앱 및 앱 데이터 삭제, 다른 기기 로그인 등의 특수 경우 발생 시 온보딩을 중도 이탈해도 온보딩을 다시 띄우지 않도록 구현

--- app/src/main/java/org/keepgoeat/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/org/keepgoeat/data/repository/AuthRepositoryImpl.kt
@@ -3,10 +3,11 @@ package org.keepgoeat.data.repository
 import org.keepgoeat.data.datasource.local.KGEDataSource
 import org.keepgoeat.data.datasource.remote.AuthDataSource
 import org.keepgoeat.data.model.request.RequestAuth
-import org.keepgoeat.data.model.response.ResponseAuth
 import org.keepgoeat.data.model.response.ResponseRefresh
 import org.keepgoeat.data.model.response.ResponseWithdraw
+import org.keepgoeat.domain.model.AuthInfo
 import org.keepgoeat.domain.repository.AuthRepository
+import org.keepgoeat.domain.type.SignType
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -14,15 +15,18 @@ class AuthRepositoryImpl @Inject constructor(
     private val authDataSource: AuthDataSource,
     private val localStorage: KGEDataSource,
 ) : AuthRepository {
-    override suspend fun login(requestAuth: RequestAuth): Result<ResponseAuth.ResponseAuthData?> =
+    override suspend fun login(requestAuth: RequestAuth): Result<AuthInfo?> =
         runCatching {
-            authDataSource.login(requestAuth).data
+            authDataSource.login(requestAuth).data?.toAuthInfo()
         }.onSuccess {
             with(localStorage) {
                 isLogin = true
                 it?.let {
                     accessToken = it.accessToken
                     refreshToken = it.refreshToken
+
+                    if (it.signType == SignType.SIGN_IN)
+                        isClickedOnboardingButton = true
                 }
             }
         }.onFailure {
